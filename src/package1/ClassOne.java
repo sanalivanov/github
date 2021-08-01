10f986eac260f2319b17f9b7448b2389335e831e
sanalivanov <sanalivanov02@gmail.com>
2021-08-01 14:42:15 -0500

    third commit

--- src/package1/ClassOne.java
+++ src/package1/ClassOne.java
@@ -5,6 +5,7 @@ public class ClassOne {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("Batch 35 and 36 learn GitHub");
+        System.out.println("We like Git but we will like it more when we will have more practice");
 
 
     }
