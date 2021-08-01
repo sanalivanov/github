7bd7dfb03110c8ee2c40fdfe627189057964472e
sanalivanov <sanalivanov02@gmail.com>
2021-08-01 14:40:18 -0500

    second commit

--- src/package1/ClassOne.java
+++ src/package1/ClassOne.java
@@ -4,6 +4,8 @@ public class ClassOne {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Batch 35 and 36 learn GitHub");
+
 
     }
 
